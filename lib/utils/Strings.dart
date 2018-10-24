211d676b71ab65371f7dc7209730f27ff0ca54b9
Mike <manc88@mail.ru>
2018-10-24 16:53:16 +0300

    drawer menu tiles

--- lib/utils/Strings.dart
+++ lib/utils/Strings.dart
@@ -1,3 +1,15 @@
 abstract class Strings {
   static const String appName = "Mistazzy";
+  static const String drawerheader = "Настройки";
+  static const String login = "Вход";
+  static const String loginDetails = "Авторизируйтесь для общения";
+  static const String topicFilter = "Фильтр разделов";
+  static const String topicFilterDetails = "Установите фильтр тем по разделам";
+  static const String personalization = "Персонализация";
+  static const String personalizationDetails =
+      "Настройка внешнего вида приложения";
+  static const String favorite = "Избранное";
+  static const String favoriteDetails = "Избранные темы";
+  static const String settings = "Настройки";
+  static const String settingsDetails = "Дополнительные настрйоки приложения";
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,38 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:mistazzy/nav/paginator.dart';
+import 'package:mistazzy/utils/Strings.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          AppBar(
-            title: Text("Menu"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, '/login');
-            },
-            leading: Icon(Icons.portrait),
-            title: Text("Login"),
-            subtitle: Text("exerimental"),
-          ),
-          ListTile(
-            onTap: () {
-              Paginator.goToTopicsList(context);
-            },
-            leading: Icon(Icons.chat_bubble_outline),
-            title: Text("Chats"),
-            subtitle: Text("Sorting, Favorits etc"),
-          ),
-          ListTile(
-            onTap: () {},
-            leading: Icon(Icons.settings),
-            title: Text("Settings"),
-            subtitle: Text("Visual, Network & others"),
-          )
-        ],
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              title: Text(Strings.drawerheader),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              leading: Icon(
+                Icons.portrait,
+                size: 32.0,
+              ),
+              title: Text(
+                Strings.login,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Text(Strings.loginDetails),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {
+                Paginator.goToTopicsList(context);
+              },
+              leading: Icon(
+                Icons.filter_list,
+                size: 32.0,
+              ),
+              title: Text(
+                Strings.topicFilter,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Text(Strings.topicFilterDetails),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.favorite_border,
+                size: 32.0,
+              ),
+              title: Text(
+                Strings.favorite,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Text(Strings.favoriteDetails),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.color_lens,
+                size: 32.0,
+              ),
+              title: Text(
+                Strings.personalization,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Text(Strings.personalizationDetails),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.settings,
+                size: 32.0,
+              ),
+              title: Text(
+                Strings.settings,
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Text(Strings.settingsDetails),
+            ),
+          ],
+        ),
       ),
     );
   }
